2456deb11ceb1a556b125dee88b8a4d640427d8c
LuanaPorciuncula <lpb@cin.ufpe.br>
2021-11-23 10:23:10 -0300

    adicionado dados do Json e Json Loader

---
+++ UIModaConsciente/DataLoader.swift
@@ -0,0 +1,30 @@
+//
+//  DataLoader.swift
+//  UIModaConsciente
+//
+//  Created by Luana Porciuncula Barreto on 23/11/21.
+//
+
+import Foundation
+
+public class DataLoader {
+    @Published var recentClothe = [RecentClothe]();
+    
+    
+    init (){
+        loadRecentData()
+    }
+    func loadRecentData() {
+        if let fileLocation = Bundle.main.url(forResource: "recent_data", withExtension: "json") {
+            do {
+                let recentData = try Data(contentsOf:fileLocation);
+                let jsonDecoder = JSONDecoder();
+                let dataFromJson = try jsonDecoder.decode([RecentClothe].self, from: recentData);
+                self.recentClothe = dataFromJson;
+            } catch {
+                print(error);
+            }
+        }
+    }
+    
+}
